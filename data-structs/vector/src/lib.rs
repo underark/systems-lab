54dd12f274ea04b90f154dbecf418fc8d2aad8ee
alex <peter.alex.morris@gmail.com>
2026-07-19 21:58:26 +0900

    Added initial v2 implementation. Main differences between v1 and v2 are checks for integer overflows on size calculations and explicit invariant over size of type (non-zero-size)

--- data-structs/vector/src/lib.rs
+++ data-structs/vector/src/lib.rs
@@ -1,140 +1,135 @@
 use std::alloc::{Layout, alloc, dealloc, handle_alloc_error};
-use std::fmt::Result as FmtResult;
+use std::error::Error;
 use std::fmt::{Debug, Formatter};
+use std::fmt::{Display, Result as FmtResult};
 use std::marker::PhantomData;
+use std::mem::{ManuallyDrop, needs_drop};
+use std::ops::DerefMut;
 use std::ptr::drop_in_place;
-
-// Must always be true:
-// length is the number of elements stored in the vector
-// 0..length elements are initialized
-// length..(capacity / size_of::<T>) elements are uninitialized
-// capacity refers to the total number of bytes assigned to the vector
-// capacity cannot exceed usize (total addressable space of the system)
-// length cannot exceed usize
+use std::{panic, vec};
 
 pub struct Vector<T> {
     start: *mut T,
-    current_layout: Layout, // dealloc() expects to be called with the same layout that was used to
-    // alloc the memory
+    layout: Layout,
     capacity: usize,
     length: usize,
     phantom: PhantomData<T>,
 }
 
-struct VectorAlloc<T> {
-    start: *mut T,
+struct VecAlloc {
+    ptr: *mut u8,
     layout: Layout,
 }
 
+#[derive(Debug)]
+struct OverflowError {}
+
+impl Display for OverflowError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "size calculation overflowed")
+    }
+}
+
+impl Error for OverflowError {}
+
 impl<T> Vector<T> {
-    pub fn new() -> Vector<T> {
-        let alloc: VectorAlloc<T> = Vector::allocate_memory(1);
+    pub fn new(elements: usize) -> Vector<T> {
+        if size_of::<T>() == 0 {
+            panic!("vector cannot be initialized with zero-size type");
+        }
+
+        let allocated = Vector::<T>::allocate_memory(elements).unwrap();
         Vector {
-            start: alloc.start,
-            current_layout: alloc.layout,
-            capacity: 1,
+            start: allocated.ptr.cast(),
+            layout: allocated.layout,
+            capacity: elements,
             length: 0,
             phantom: PhantomData,
         }
     }
 
+    // length <= capacity
+    // elements 0..length - 1 are initialized
+    // elements length..capacity - 1 are uninitialized
+    // absolute max number of elements is <= isize::MAX / size_of::<T>()
     pub fn push(&mut self, t: T) {
-        // returning early prevents allocations for zero-sized types
-        if size_of::<T>() == 0 {
-            self.length += 1;
-            return;
+        if self.length == self.capacity {
+            self.migrate_vector().unwrap();
         }
 
-        if self.length >= self.capacity {
-            self.migrate_vector();
-        }
-
-        // SAFETY: be careful of off by one errors. write to self.length; read from self.length - 1
         unsafe {
             self.start.add(self.length).write(t);
+            self.length += 1;
         }
-
-        self.length += 1;
     }
 
     pub fn pop(&mut self) -> Option<T> {
         if self.length == 0 {
-            return None;
-        }
-
-        // SAFETY: be careful of off by one errors. write to self.length; read from self.length - 1
-        // SAFETY: T returned from pop is moved out of allocation in vector but still exists in
-        // memory; self.length + 1 must be treated as uninitialized
-        unsafe {
+            None
+        } else {
             self.length -= 1;
-            let p = self.start.add(self.length).read();
-            Some(p)
+            unsafe { Some(self.start.add(self.length).read()) }
         }
     }
 
-    // NOTE: it is possible for this function to fail, but no Result<> type is returned because all
-    // failure results in unwinding
-    fn allocate_memory(size: usize) -> VectorAlloc<T> {
-        // SAFETY: clamping at 1 ensures that zero-sized types never request 0 bytes, which is UB
-        let t_size = size_of::<T>().min(1);
-        let align = align_of::<T>();
-
-        // from_size_align will panic at unwrap() if certain conditions are untrue (see docs)
-        // this is only the case for values passed into from_size_align i.e. prior calculations must
-        // not overflow, or else this will not be caught
-        let l = Layout::from_size_align(t_size.saturating_mul(size), align).unwrap();
-        // SAFETY: pointer returned from alloc may be null. Calling handle_alloc_error for failed states
+    // question: how am i meeting allocator contracts?
+    fn allocate_memory(elements: usize) -> Result<VecAlloc, Box<dyn Error>> {
+        let size = elements
+            .checked_mul(size_of::<T>())
+            .ok_or(OverflowError {})?;
+        let layout = Layout::from_size_align(size, align_of::<T>())?;
         unsafe {
-            let p = alloc(l);
-            if p.is_null() {
-                handle_alloc_error(l);
-            } else {
-                VectorAlloc {
-                    start: p.cast(),
-                    layout: l,
-                }
+            let ptr = alloc(layout);
+            if ptr.is_null() {
+                handle_alloc_error(layout);
             }
+            Ok(VecAlloc { ptr, layout })
         }
     }
 
-    fn migrate_vector(&mut self) {
-        let new_size = self.length.saturating_mul(2);
-        let alloc: VectorAlloc<T> = Vector::allocate_memory(new_size);
-
-        // SAFETY: iterating from 0..length - 1 because these elements should be treated as
-        // initialized - n >= length elements may contain moved out data
-        // SAFETY: using previously stored layout ensures matching layout for a given alloc is used
+    fn migrate_vector(&mut self) -> Result<(), Box<dyn Error>> {
+        let new_size = self.capacity.checked_mul(2).ok_or(OverflowError {})?;
+        let new_allocation = Vector::<T>::allocate_memory(new_size)?;
+        // question: after this returns: who owns the elements?
+        self.move_elements_to(new_allocation.ptr);
         unsafe {
+            dealloc(self.start.cast(), self.layout);
+        }
+        self.update_vec_metadata(new_size, new_allocation);
+        Ok(())
+    }
+
+    fn move_elements_to(&mut self, new_ptr: *mut u8) {
+        unsafe {
+            let t_ptr: *mut T = new_ptr.cast();
             for i in 0..self.length {
-                alloc.start.add(i).write(self.start.add(i).read());
+                t_ptr.add(i).write(self.start.add(i).read());
             }
-            dealloc(self.start.cast(), self.current_layout);
         }
-        self.start = alloc.start;
-        self.current_layout = alloc.layout;
-        self.capacity = new_size;
+    }
+
+    fn update_vec_metadata(&mut self, new_cap: usize, new_alloc: VecAlloc) {
+        self.capacity = new_cap;
+        self.start = new_alloc.ptr.cast();
+        self.layout = new_alloc.layout;
     }
 }
 
 impl<T: Debug> Debug for Vector<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        unsafe {
-            f.debug_list()
-                // NOTE: a zero-sized type will never actually increment the pointer (count *
-                // size_of::<T>()) but print the first and only element n times
-                .entries((0..self.length).map(|i| &*self.start.add(i)))
-                .finish()
-        }
+        f.debug_list()
+            .entries((0..self.length).map(|i| unsafe { &*self.start.add(i) }))
+            .finish()
     }
 }
 
 impl<T> Drop for Vector<T> {
     fn drop(&mut self) {
-        for i in 0..self.length {
-            unsafe {
+        unsafe {
+            for i in 0..self.length {
                 drop_in_place(self.start.add(i));
             }
+            dealloc(self.start.cast(), self.layout);
         }
-        unsafe { dealloc(self.start.cast(), self.current_layout) };
     }
 }
--- data-structs/vector/src/main.rs
+++ data-structs/vector/src/main.rs
@@ -1,6 +1,22 @@
 use vector::Vector;
 
 fn main() {
-    let mut v: Vec<u8> = Vec::new();
-    println!("{}", v.pop());
+    let mut v: Vector<PanicDrop> = Vector::new(5);
+    let first = PanicDrop { i: 1 };
+    let second = PanicDrop { i: 2 };
+    let third = PanicDrop { i: 3 };
+    v.push(first);
+    v.push(second);
+    v.push(third);
+    drop(v);
+}
+
+struct PanicDrop {
+    i: u8,
+}
+
+impl Drop for PanicDrop {
+    fn drop(&mut self) {
+        panic!("{:?} panicking!", self.i);
+    }
 }
